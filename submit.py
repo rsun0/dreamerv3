d397cb580a128c2a3b9e3e80bb188561c66cd6d8
Ray Sun <raysun@mllab.snu.ac.kr>
2025-02-07 10:35:04 +0900

    submit.py for 5 seeds of ale-py 0.9.0

--- submit.py
+++ submit.py
@@ -5,38 +5,16 @@ def run_exp():
     base_cmd = "python -B dreamerv3/main.py"
 
     param_dict = {
-        "--logdir": ["/storage/raysun/dreamerv3/full_run_{task}_{seed}"],
+        "--logdir": ["/storage/raysun/dreamerv3/ale9_{task}_{seed}"],
         "--configs": ["atari100k"],
-        "--run.eval_eps": [100],
         "--run.log_every": [1000],
         "--run.report_every": [10000],
-        "--run.save_every": [100000],
+        "--run.save_every": [0],
+        "--task": ["atari100k_pong"],
         "--seed": range(0, 5),
-        "--task": [
-            "atari100k_alien",
-            "atari100k_bank_heist",
-            "atari100k_breakout",
-            "atari100k_chopper_command",
-            "atari100k_crazy_climber",
-            "atari100k_demon_attack",
-            "atari100k_freeway",
-            "atari100k_frostbite",
-            "atari100k_gopher",
-            "atari100k_hero",
-            "atari100k_jamesbond",
-            "atari100k_kangaroo",
-            "atari100k_krull",
-            "atari100k_kung_fu_master",
-            "atari100k_ms_pacman",
-            "atari100k_private_eye",
-            "atari100k_qbert",
-            "atari100k_road_runner",
-            "atari100k_seaquest",
-            "atari100k_up_n_down",
-        ],
         "--logger.outputs": ["jsonl wandb"],
     }
-    job_name = "full_run"
+    job_name = "ale9"
 
     launch_tasks(
         param_option=1,
